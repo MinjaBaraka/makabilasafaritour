47046fbb81333b1d6d3489ad34842f5bdd1bea90
MinjaBaraka <45535420+MinjaBaraka@users.noreply.github.com>
2024-02-18 19:06:52 +0300

    18/02/2024
    updated safari tour

--- src/components/SafariTour/CompareTour.vue
+++ src/components/SafariTour/CompareTour.vue
@@ -85,102 +85,102 @@ export default {
 }
 </script>
 <template>
-  <section class="">
+  <section class="my-10">
+     
+    <div class="container flex flex-col xl:flex-row items-center mx-auto text-custom-gray space-x-6 px-4 md:px-0">
 
-        <!-- Open Container with width of 60% -->
-        <div class="flex justify-between">
-           <div class="container flex flex-col xl:w-3/5 xl:mx-48 mx-auto text-custom-gray px-4">
-             <h1 class="text-2xl font-bold mb-6">Tanzania Safari Tours - Compare Safari</h1>
+        <div class="flex flex-col  mx-auto text-custom-gray px-4 md:px-0">
+                     <h1 class="text-2xl font-bold mb-6">Tanzania Safari Tours - Compare Safari</h1>
 
 
-                <!-- Open 1 - 50 of 50 Tanzania Safari Tours -->
-                <div class="flex flex-row justify-between items-center mb-8">
-                    <p class="text-sm">1 - 50 of 50 Tanzania Safari Tours</p>
-                    <div class="flex items-center md:space-x-6">
-                        <p class="font-bold">Search filters:</p>
-                        <button class="outline-none text-red-500 font-extrabold capitalize ring-2 ring-red-500 rounded-lg px-6 py-2">
-                            clear filters
-                        </button>
-                    </div>
-                </div>
-                <!-- Close 1 - 50 of 50 Tanzania Safari Tours -->
-
-                <div class="flex flex-col items-center justify-center md:grid grid-cols-2 mb-10 gap-10">
-                    <div class="flex flex-col justify-center w-full h-96"  v-for="object, objectIndex in compareTour" :key="objectIndex">
-                        <div class="w-full h-64 mb-4">
-                            <img :src="object.urlImage" alt="Safari Tour" class="w-full h-full object-cover rounded-lg">
+                        <!-- Open 1 - 50 of 50 Tanzania Safari Tours -->
+                        <div class="flex flex-row justify-between items-center mb-8">
+                            <p class="text-sm">1 - 50 of 50 Tanzania Safari Tours</p>
+                            <div class="flex items-center md:space-x-6">
+                                <p class="font-bold">Search filters:</p>
+                                <button class="outline-none text-red-500 font-extrabold capitalize ring-2 ring-red-500 rounded-lg px-6 py-2">
+                                    clear filters
+                                </button>
+                            </div>
                         </div>
+                        <!-- Close 1 - 50 of 50 Tanzania Safari Tours -->
 
-                        <!-- Open Money Description -->
-                        <h2 class="text-xl font-bold text-green-500 mb-2">{{ object.moneyDescriptio }}</h2>
-                        <!-- Close Money Description -->
-
-                        <!-- Open Navigator -->
-                        <nav class="flex mb-2" aria-label="Breadcrumb">
-                            <ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
-                                <li class="inline-flex items-center hover:text-green-500 capitalize cursor-pointer">
-                                    <component :is="AngleRight" class="fill-gray-500 w-6 h-6"/>
-                                        Private tour
-                                </li>
-                                <li>
-                                <div class="flex items-center hover:text-green-500 capitalize cursor-pointer">
-                                    <component :is="AngleRight" class="fill-gray-500 w-6 h-6"/>
-                                    Luxury tour
+                        <div class="container flex flex-col items-center justify-center md:grid xl:grid-cols-3 grid-cols-2 mb-10 gap-10">
+                            <div class="flex flex-col justify-center w-full h-96"  v-for="object, objectIndex in compareTour" :key="objectIndex">
+                                <div class="w-full h-64 mb-4">
+                                    <img :src="object.urlImage" alt="Safari Tour" class="w-full h-full object-cover rounded-lg">
                                 </div>
-                                </li>
-                                 <li>
-                                    <div class="flex items-center hover:text-green-500 capitalize cursor-pointer">
-                                    <component :is="AngleRight" class="fill-gray-500 w-6 h-6"/>
-                                       Hotel
-                                    </div>
-                                    </li>
-                            </ol>
-                         </nav>
-                        <!-- Close Navigator -->
 
-                        <!-- Open Visited -->
-                        <p class="mb-2"><span class="font-bold">You Visited:</span> {{ object.visited }}</p>
-                        <!-- Close Visited -->
-                    </div>
-                </div>
-           </div>
-           <!-- Close Container with width of 60% -->
+                                <!-- Open Money Description -->
+                                <h2 class="text-xl font-bold text-green-500 mb-2">{{ object.moneyDescriptio }}</h2>
+                                <!-- Close Money Description -->
 
-           <!-- Open Remain Container of 60% -->
-   
-               <div class="hidden xl:flex flex-col space-y-10 mr-10 xl:w-1/4">
+                                <!-- Open Navigator -->
+                                <nav class="flex mb-2" aria-label="Breadcrumb">
+                                    <ol class="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
+                                        <li class="inline-flex items-center hover:text-green-500 capitalize cursor-pointer">
+                                            <component :is="AngleRight" class="fill-gray-500 w-6 h-6"/>
+                                                Private tour
+                                        </li>
+                                        <li>
+                                        <div class="flex items-center hover:text-green-500 capitalize cursor-pointer">
+                                            <component :is="AngleRight" class="fill-gray-500 w-6 h-6"/>
+                                            Luxury tour
+                                        </div>
+                                        </li>
+                                         <li>
+                                            <div class="flex items-center hover:text-green-500 capitalize cursor-pointer">
+                                            <component :is="AngleRight" class="fill-gray-500 w-6 h-6"/>
+                                               Hotel
+                                            </div>
+                                            </li>
+                                    </ol>
+                                 </nav>
+                                <!-- Close Navigator -->
 
-                    <!-- Open Your Safari Tour. -->
-                   <FormSafariTour />
-                   <!-- Open Your Safari Tour. -->
+                                <!-- Open Visited -->
+                                <p class="mb-2"><span class="font-bold">You Visited:</span> {{ object.visited }}</p>
+                                <!-- Close Visited -->
+                            </div>
+                        </div>
+                   </div>
 
-                   <!-- Open Tour Range -->
-                   <TourRange />
-                   <!-- Close Tour Range -->
-
-
-                   <!-- Open Rate in USD -->
-                       <RateUSD />
-                    <!-- Close Rate in USD -->
-
-
-                    <!-- Open Comfort Type -->
-                    <ComfortType />
-                    <!-- Close Comfort Type -->
-
-
-                    <!-- Open Operator Rating -->
-                    <OperatorRating />
-                    <!-- Close Operator Rating -->
-
-                    <!-- Open Specilized Tour -->
-                    <SpecilizedTour />
-                    <!-- Close Specilized Tour -->
+        
                    
-               </div>
-   
-           <!-- Close Remain Container of 60% -->
-        </div>
+                     <div class="container flex flex-col mx-auto space-y-10 xl:w-1/4 px-4 md:px-0">
 
+                            <!-- Open Your Safari Tour. -->
+                           <FormSafariTour />
+                           <!-- Open Your Safari Tour. -->
+
+                           <!-- Open Tour Range -->
+                           <TourRange />
+                           <!-- Close Tour Range -->
+
+
+                           <!-- Open Rate in USD -->
+                               <RateUSD />
+                            <!-- Close Rate in USD -->
+
+
+                            <!-- Open Comfort Type -->
+                            <ComfortType />
+                            <!-- Close Comfort Type -->
+
+
+                            <!-- Open Operator Rating -->
+                            <OperatorRating />
+                            <!-- Close Operator Rating -->
+
+                            <!-- Open Specilized Tour -->
+                            <SpecilizedTour />
+                            <!-- Close Specilized Tour -->
+
+                       </div>
+
+      
+
+    </div>
+       
   </section>
 </template>
--- src/components/SafariTour/SideCompareTour/FormSafariTour.vue
+++ src/components/SafariTour/SideCompareTour/FormSafariTour.vue
@@ -13,25 +13,25 @@ export default {
 }
 </script>
 <template>
-    <div class="bg-white shadow-lg h-fit">
-        <div class="flex flex-col items-center mx-auto my-4">
+    <div class="relative container flex mx-auto bg-white shadow-md rounded-md  h-fit w-full">
+        <div class="flex flex-col items-center mx-auto my-4 w-full">
             <h1 class="text-2xl font-bold text-custom-gray">Your Safari Tour.</h1>
 
-            <form action="" class="flex flex-col items-center space-y-6 my-10">
+            <form action="" class="flex flex-col items-center space-y-6 my-10 px-4 md:px-0 w-full">
                  <!-- Where to... -->
-                <label for="whereto" class="flex  rounded-lg items-center p-2">
+                <label for="whereto" class="relative flex  rounded-lg items-center p-2 w-full">
                     <input type="text" id="whereto" placeholder="Where to..." class="outline-none border-2 border-custom-green focus:border-custom-green py-3 px-3 w-full rounded-lg">
                     <component :is="Location" class="absolute right-[8%] w-6 h-6 fill-custom-green"/>
                  </label>
 
                  <!-- Date... -->
-                 <label for="StartDate" class="flex  rounded-lg items-center p-2">
+                 <label for="StartDate" class="relative flex rounded-lg items-center p-2 w-full">
                     <input datepicker type="text" id="date" class="outline-none border-2 border-custom-green focus:border-custom-green py-3 px-3 w-full rounded-lg" placeholder="Select date">
                     <component :is="Calendar" class="absolute right-[8%] w-6 h-6 fill-custom-green"/>
                 </label>
 
                  <!-- Travellers... -->
-                <label for="travellers" class="flex  rounded-lg items-center p-2">
+                <label for="travellers" class="relative flex rounded-lg items-center p-2 w-full">
                     <input type="text" id="travellers" placeholder="Travellers..." class="outline-none border-2 border-custom-green focus:border-custom-green py-3 px-3 w-full rounded-lg">
                     <component :is="Briefcase" class="absolute right-[8%] w-6 h-6 fill-custom-green"/>
                 </label>
