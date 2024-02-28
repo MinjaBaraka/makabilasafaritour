3982d8b7ae6d984a4eb60eeb02c2b8495c66db4a
MinjaBaraka <45535420+MinjaBaraka@users.noreply.github.com>
2024-02-28 09:24:11 +0300

    28/02/2024
    Update SVG Icons

--- src/components/included/NavBar.vue
+++ src/components/included/NavBar.vue
@@ -1,23 +1,27 @@
 <script>
 import { ref } from 'vue'
 
-import { Search, Menu } from '../../components/constant/svg'
+import { Search, Menu, xLG } from '../../components/constant/svg'
 export default {
     setup () {
 
+        const open = ref(false)
+
         const menu = ref([
-            { title: 'Home', link: '/' },
-            { title: 'About Us', link: '/AboutUs' },
-            { title: 'Tours', link: '/Tours' },
-            { title: 'Parks', link: '' },
-            { title: 'Operators', link: '' },
-            { title: 'Plan with AI', link: '' },
+            { title: 'Home', link: '/', },
+            { title: 'About Us', link: '/AboutUs',  },
+            { title: 'Tours', link: '/Tours',  },
+            { title: 'Parks', link: '',  },
+            { title: 'Operators', link: '',  },
+            { title: 'Plan with AI', link: '',  },
         ])
 
         return {
             menu,
             Search,
-            Menu
+            Menu,
+            xLG,
+            open
         }
 
     }
@@ -39,7 +43,7 @@ export default {
                 <div class="hidden xl:flex items-center space-x-8">
                     <ul class="outline-none" v-for="object, objectIndex in menu" :key="objectIndex">
                         <li class="cursor-pointer ">
-                            <router-link :to="object.link" class="hover:text-green-500 animation_text">{{ object.title }}</router-link>
+                            <router-link :to="object.link" class="hover:text-green-500 animation_text focus:text-green-500">{{ object.title }}</router-link>
                         </li>
                     </ul>
                 </div>
@@ -56,32 +60,34 @@ export default {
     
                 <!-- Mobile Menu Icon -->
                 <div class="xl:hidden block">
-                    <component :is="Menu" class="fill-current stroke-green-500"/>
+                    <component :is="Menu" class=" w-10 h-10 fill-custom-green"/>
                 </div>
             </nav>
-        </div>
 
-        <div class="hidden">
-            <!-- Mobile List Menu -->
-            <div class="xl:hidden flex flex-col items-center absolute bg-white w-full ">
-               <div class="space-y-6 mb-10">
-                 <ul class="outline-none md:text-3xl text-2xl" v-for="object, objectIndex in menu" :key="objectIndex">
-                           <li class="cursor-pointer">
-                               <a href="" class="hover:text-green-500 ease-in duration-300 hover:underline decoration-green-500">{{ object.title }}</a>
-                           </li>
-                   </ul>
-               </div>
+             <div class="hidden">
+                <!-- Mobile List Menu -->
+                <div class="xl:hidden flex flex-col items-center absolute bg-white w-full ">
+                   <div class="space-y-6 mb-10">
+                     <ul class="outline-none md:text-3xl text-2xl" v-for="object, objectIndex in menu" :key="objectIndex">
+                               <li class="cursor-pointer">
+                                   <router-link :to="object.link" class="hover:text-green-500 animation_text focus:text-green-500">{{ object.title }}</router-link>
+                               </li>
+                       </ul>
+                   </div>
 
-                           <!-- Search -->
-                    <div class="xl:hidden flex mb-10">
-                        <form action="" class="flex space-x-4">
-                            <label for="search" class="relative flex rounded-lg items-center p-2 cursor-pointer">
-                                <component :is="Search" class="absolute right-[10%] w-6 h-6 fill-custom-green"/>
-                                <input  type="text" placeholder="search..." id="date" class="outline-none border-2 border-custom-green focus:border-custom-green py-3 px-3 w-full rounded-lg">
-                            </label>
-                        </form>
-                    </div>
+                               <!-- Search -->
+                        <div class="xl:hidden flex mb-10">
+                            <form action="" class="flex space-x-4">
+                                <label for="search" class="relative flex rounded-lg items-center p-2 cursor-pointer">
+                                    <component :is="Search" class="absolute right-[10%] w-6 h-6 fill-custom-green"/>
+                                    <input  type="text" placeholder="search..." id="date" class="outline-none border-2 border-custom-green focus:border-custom-green py-3 px-3 w-full rounded-lg">
+                                </label>
+                            </form>
+                        </div>
+                </div>
             </div>
         </div>
+
+       
     </header>
 </template>
